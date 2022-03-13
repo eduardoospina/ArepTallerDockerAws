ae37cd698dd80a34974f8da9de6f4c601902d085
eduardoospina <eduardo.ospina@mail.escuelaing.edu.co>
2022-03-13 16:41:01 -0500

    Creacion de sparkwebServer, agregando necesidades del POM

---
+++ src/main/java/co/edu/escuelaing/arep/SparkWebServer.java
@@ -0,0 +1,23 @@
+package co.edu.escuelaing.arep;
+
+import static spark.Spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class SparkWebServer {
+
+    public static void main(String... args){
+        port(getPort());
+        get("hello", (req,res) -> "Hello Docker!");
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+
+}
