f04d69b4db2ff26e17b869c37215d9a05b04dec5
eduardoospina <eduardo.ospina@mail.escuelaing.edu.co>
2022-03-16 01:55:16 -0500

    avance app.js

---
+++ tareaImpl/RoundRobin/src/main/resources/static/js/app.js
@@ -0,0 +1,19 @@
+var app = (function(){
+
+    var URLE = "http//localhost:34999/agregar?word=";
+
+    function enviarMensaje(){
+        var mensajeAInsertar = document.getElementById("inputName").value;
+        console.info(mensajeAInsertar)
+        $.ajax({
+            url: URLE + mensajeAInsertar;
+            type:'POST'
+        }).then(function(data){
+           alert(response.status)
+        });
+    }
+
+    return{
+        enviar:enviarMensaje
+    };
+})();
